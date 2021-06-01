a7e61f1bdbc63900c9d9afed2333f497fd3cb6e5
Victor Norman <vtn2@calvin.edu>
2021-06-01 14:16:41 -0400

    icons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gifty
+# Gift Xchangr
 
 This is an Ionic app which allows members of a family to add their (Christmas) gift lists. Others in the family can see the lists, claim a gift, comment on them, etc., but the
 person the gift is for cannot see it.
---
+++ build_icons.sh
@@ -0,0 +1,16 @@
+cp resources/icon.png resources/android/icon-foreground.png
+
+cp resources/android/icon/hdpi-foreground.png android/app/src/main/res/mipmap-hdpi/ic_launcher_foreground.png
+cp resources/android/icon/hdpi-foreground.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+
+cp resources/android/icon/xxxhdpi-foreground.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_foreground.png
+cp resources/android/icon/xxxhdpi-foreground.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+
+cp resources/android/icon/xxhdpi-foreground.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher_foreground.png
+cp resources/android/icon/xxhdpi-foreground.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+
+cp resources/android/icon/xhdpi-foreground.png android/app/src/main/res/mipmap-xhdpi/ic_launcher_foreground.png
+cp resources/android/icon/xhdpi-foreground.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+
+cp resources/android/icon/mdpi-foreground.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp resources/android/icon/mdpi-foreground.png android/app/src/main/res/mipmap-mdpi/ic_launcher_foreground.png
